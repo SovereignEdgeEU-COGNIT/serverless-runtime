a0a6056ead5edc2e6637c97e1b391bdc0547eef8
Álvaro Puente <apuente@ikerlan.es>
2025-04-15 11:25:23 +0200

    improving entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# PID file
+PID_FILE="./uvicorn.pid"
+
 # Kill existing Uvicorn process if running
-[ -f uvicorn.pid ] && kill $(cat uvicorn.pid) 2>/dev/null && echo "Previous process killed" || echo "No running process to kill"
+if [ -f "$PID_FILE" ]; then
+  kill $(cat "$PID_FILE") 2>/dev/null && echo "Previous process killed" || echo "Failed to kill previous process"
+  rm -f "$PID_FILE"
+else
+  echo "No running process to kill"
+fi
 
 # Source the one_env file to load variables
 source /var/run/one-context/one_env
 
-# Get the COGNIT_BROKER and COGNIT_FLAVOUR from env, or fail if not set
+# Check required environment variables
 if [[ -z "$COGNIT_BROKER" || -z "$COGNIT_FLAVOUR" ]]; then
   echo "COGNIT_BROKER or COGNIT_FLAVOUR is not set."
+  exit 1
 fi
 
-# Start the API with Uvicorn
-python3 main.py --host "0.0.0.0" --port 8000 --broker "$COGNIT_BROKER" --flavour "$COGNIT_FLAVOUR" & echo $! > ../uvicorn.pid &
+# Start the API with Uvicorn and save the PID
+python3 main.py --host "0.0.0.0" --port 8000 --broker "$COGNIT_BROKER" --flavour "$COGNIT_FLAVOUR" &
+echo $! > "$PID_FILE"
+
